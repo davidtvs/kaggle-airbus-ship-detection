11b7025e7bad118d970659a4f67ae5eca23d68e5
David Silva <davidtvs10@gmail.com>
2018-10-29 21:52:28 +0000

    Rewrite train_model() into Trainer class

--- src/train.py
+++ src/train.py
@@ -13,100 +13,115 @@ from data.airbus import AirbusShipDataset
 from transforms import TargetHasShipTensor
 
 
-def train_model(model, dataloaders, criterion, optimizer):
-    """Trains a model on the training set and evaluates it on the validation set.
+class Trainer:
+    def __init__(self, model, args):
+        self.args = args
+        self.device = torch.device(self.args.device)
+        self.model = model.to(self.device)
 
-    The model is trained on the training dataset (`dataloaders['train']`), minimizing
-    `criterion` using `optimizer` as the optimization algorithm. In each epoch, after
-    training, the model is evaluated on the validation set (`dataloaders['val']`).
+        # Loss function: binary cross entropy with logits. Expects logits therefore the
+        # output layer must return a logits instead of probabilities
+        self.criterion = torch.nn.BCEWithLogitsLoss()
 
-    Arguments:
-        model (torch.nn.Module): the model instance to train.
-        dataloaders (torch.utils.data.Dataloader): Provides single or multi-process
-            iterators over the dataset.
-        criterion (torch.nn): The loss criterion.
-        optimizer (torch.optim): The optimization algorithm.
-
-    """
-    since = time.time()
-
-    device = torch.device(args.device)
-    model = model.to(device)
-
-    # Initialize metrics
-    val_acc_history = []
-    best_model = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-
-    # Training cycle
-    print()
-    for epoch in range(args.epochs):
-        print("Epoch {}/{}".format(epoch, args.epochs - 1))
-        print("-" * 80)
-
-        # Each epoch has a training and validation phase
-        for phase in ["train", "val"]:
-            if phase == "train":
-                # Set model to training mode
-                model.train()
-            else:
-                # Set model to evaluate mode
-                model.eval()
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for step, (inputs, labels) in enumerate(tqdm(dataloaders[phase])):
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == "train"):
-                    outputs = model(inputs)
-                    loss = criterion(outputs, labels)
-
-                    preds = torch.sigmoid(outputs).round_()
-
-                    # backward + optimize only if in training phase
-                    if phase == "train":
-                        loss.backward()
-                        optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-
-            epoch_loss = running_loss / len(dataloaders[phase].dataset)
-            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
-
-            print("{} Loss: {:.4f} Acc: {:.4f}".format(phase, epoch_loss, epoch_acc))
-
-            # deep copy the model
-            if phase == "val" and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model = copy.deepcopy(model.state_dict())
-                utils.save_checkpoint(model, optimizer, epoch, best_acc, args)
-            if phase == "val":
-                val_acc_history.append(epoch_acc)
-
-        print()
-
-    time_elapsed = time.time() - since
-    print(
-        "Training complete in {:.0f}m {:.0f}s".format(
-            time_elapsed // 60, time_elapsed % 60
+        self.optimizer = torch.optim.Adam(
+            self.model.parameters(), lr=self.args.learning_rate
         )
-    )
-    print("Best val Acc: {:4f}".format(best_acc))
 
-    # load best model weights
-    model.load_state_dict(best_model)
-    return model, val_acc_history
+    def make_epoch(self, dataloader, epoch, is_training):
+        # Set model to training mode if training; otherwise, set it to evaluation mode
+        if is_training:
+            self.model.train()
+        else:
+            self.model.eval()
+
+        running_loss, running_corrects = self.make_step(dataloader, is_training)
+
+        epoch_loss = running_loss / len(dataloader.dataset)
+        epoch_acc = running_corrects.double() / len(dataloader.dataset)
+
+        return epoch_loss, epoch_acc
+
+    def make_step(self, dataloader, is_training):
+        # Initialize running metrics
+        running_loss = 0.0
+        running_corrects = 0
+
+        # Iterate over data.
+        for step, (inputs, labels) in enumerate(tqdm(dataloader)):
+            inputs = inputs.to(self.device)
+            labels = labels.to(self.device)
+
+            # Zero the parameter gradients
+            self.optimizer.zero_grad()
+
+            # Forward
+            # Track history only if training
+            with torch.set_grad_enabled(is_training):
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+
+                # Apply the sigmoid function to get the prediction from the logits
+                preds = torch.sigmoid(outputs).round_()
+
+                # Backward only if training
+                if is_training:
+                    loss.backward()
+                    self.optimizer.step()
+
+            # statistics
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+
+        return running_loss, running_corrects
+
+    def fit(self, train_dataloader, val_dataloader):
+        # Get the current time to know how much time it took to train the model
+        since = time.time()
+
+        val_acc_history = []
+        best_acc = 0.0
+        best_model = copy.deepcopy(self.model.state_dict())
+
+        # Save the model (mostly to find out if the saving process succeeds)
+        utils.save_checkpoint(self.model, self.optimizer, best_acc, 0, self.args)
+
+        # Start training the model
+        for epoch in range(self.args.epochs):
+            print("Epoch {}/{}".format(epoch, self.args.epochs - 1))
+            print("-" * 80)
+
+            epoch_loss, epoch_acc = self.make_epoch(
+                train_dataloader, epoch, is_training=True
+            )
+            print("Training - Loss: {:.4f} Acc: {:.4f}".format(epoch_loss, epoch_acc))
+
+            epoch_loss, epoch_acc = self.make_epoch(
+                val_dataloader, epoch, is_training=False
+            )
+            print("Validation - Loss: {:.4f} Acc: {:.4f}".format(epoch_loss, epoch_acc))
+
+            # Deep copy the model
+            if epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model = copy.deepcopy(self.model.state_dict())
+                utils.save_checkpoint(
+                    self.model, self.optimizer, epoch, best_acc, self.args
+                )
+
+            val_acc_history.append(epoch_acc)
+            print()
+
+        time_elapsed = time.time() - since
+        print(
+            "Training complete in {:.0f}m {:.0f}s".format(
+                time_elapsed // 60, time_elapsed % 60
+            )
+        )
+        print("Best val Acc: {:4f}".format(best_acc))
+
+        # Load the best model weights
+        self.model.load_state_dict(best_model)
+        return self.model, val_acc_history
 
 
 # Run only if this module is being run directly
@@ -158,14 +173,7 @@ if __name__ == "__main__":
     if args.dataset_info:
         utils.dataloader_info(val_loader)
 
-    dataloaders = {"train": train_loader, "val": val_loader}
-
-    # Loss function: binary cross entropy with logits. Expects logits therefore the
-    # output layer must return a logits instead of probabilities
-    criterion = torch.nn.BCEWithLogitsLoss()
-
-    # Optimizer: adam
-    optimizer = torch.optim.Adam(snsnet.parameters(), lr=args.learning_rate)
-
     # Train the model
-    train_model(snsnet, dataloaders, criterion, optimizer)
+    print()
+    train = Trainer(snsnet, args)
+    train.fit(train_loader, val_loader)
